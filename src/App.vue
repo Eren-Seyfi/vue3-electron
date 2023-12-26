f4bcf53def7003896456fcf044089c6338d942ce
Eren Seyfi <45491369+Eren-Seyfi@users.noreply.github.com>
2023-12-26 04:02:31 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+    <h1>test vue </h1>
+  </template>
+  
+  <script setup>
+  console.log('👋 This message is being logged by "App.vue", included via Vite');
+  </script>
